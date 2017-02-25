2bfd880a7e960075342adbe7cd9ed6dc40f8a13f
oleno <pirit.lena@gmail.com>
2017-02-25 17:26:02 +0200

    Recipe 02. Passing Data Between Activities (initial)

---
+++ R02_PassingData_Between_Activities/app/src/main/java/com/prt/r02_passingdata_between_activities/MainActivity.java
@@ -0,0 +1,13 @@
+package com.prt.r02_passingdata_between_activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
