c937adc9e02612f1e1fa6d84f9753396c3629201
oleno <pirit.lena@gmail.com>
2017-02-25 20:02:30 +0200

    Recipe 02. Passing Data Between Activities (passing back to main activity)

--- R02_PassingData_Between_Activities/app/src/main/java/com/prt/r02_passingdata_between_activities/MainActivity.java
+++ R02_PassingData_Between_Activities/app/src/main/java/com/prt/r02_passingdata_between_activities/MainActivity.java
@@ -1,10 +1,11 @@
 package com.prt.r02_passingdata_between_activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    // Passing the data to SecondActivity
     public void onClick(View view) {
         Intent intent = new
                 Intent("com.prt.r02_passingdata_between_activities.SecondActivity");
@@ -34,4 +36,24 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, 1);
     }
 
+    // Getting back the data from SecondActivity
+    public void onActivityResult(int requestCode,
+                                 int resultCode,
+                                 Intent data) {
+        // check if the request code is 1
+        if (requestCode == 1) {
+            // if the result is OK
+            if (resultCode == RESULT_OK) {
+                // get the result using getIntExtra()
+                Toast.makeText(this, Integer.toString(
+                        data.getIntExtra("age3", 0)),
+                        Toast.LENGTH_SHORT).show();
+
+                // get the result using getData()
+                Uri url = data.getData();
+                Toast.makeText(this, url.toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
--- R02_PassingData_Between_Activities/app/src/main/java/com/prt/r02_passingdata_between_activities/SecondActivity.java
+++ R02_PassingData_Between_Activities/app/src/main/java/com/prt/r02_passingdata_between_activities/SecondActivity.java
@@ -1,7 +1,10 @@
 package com.prt.r02_passingdata_between_activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
@@ -11,6 +14,7 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+    // Getting the data from MainActivity
         // get the data passed in using getStringExtra()
         Toast.makeText(this, getIntent().getStringExtra("str1"),
                 Toast.LENGTH_SHORT).show();
@@ -31,6 +35,23 @@ public class SecondActivity extends AppCompatActivity {
         // get the data using the getInt() method
         Toast.makeText(this, Integer.toString(bundle.getInt("age2")),
                 Toast.LENGTH_SHORT).show();
+    }
 
+
+    // Passing the data back to MainActivity
+    public void onClick(View view) {
+        // use an Intent object to return data
+        Intent intent = new Intent();
+
+        // use the putExtra() method to return some value
+        intent.putExtra("age3", 45);
+
+        // use the setData() method to return some value
+        intent.setData(Uri.parse("https://github.com/PrtLn/Android_Cookbook_Recipes"));
+
+        // set the result with OK and the Intent object
+        setResult(RESULT_OK, intent);
+
+        finish();
     }
 }
