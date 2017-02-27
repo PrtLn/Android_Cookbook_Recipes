67c7a36e90ce40f738f63673c8e787afb1984d3b
oleno <pirit.lena@gmail.com>
2017-02-27 10:00:45 +0200

    Recipe 06. Auto-launching An Application At Boot Time (initial)

---
+++ R06_Auto-Launching_App_At_Boot_Time/app/src/main/java/com/prt/r06_auto_launching_app_at_boot_time/MainActivity.java
@@ -0,0 +1,13 @@
+package com.prt.r06_auto_launching_app_at_boot_time;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
