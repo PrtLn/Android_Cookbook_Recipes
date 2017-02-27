3f5aa8096a3c2fed865a013ed88fe88c818cc076
oleno <pirit.lena@gmail.com>
2017-02-27 09:23:17 +0200

    Recipe 05. Assigning Priorities to Broadcast Receivers (initial)

---
+++ R05_Assigning_Priorities_to_Broadcast_Receivers/app/src/main/java/com/prt/r05_assigning_priorities_to_broadcast_receivers/MainActivity.java
@@ -0,0 +1,13 @@
+package com.prt.r05_assigning_priorities_to_broadcast_receivers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
