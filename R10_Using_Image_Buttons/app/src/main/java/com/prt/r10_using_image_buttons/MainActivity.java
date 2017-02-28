ec93ae360d307e029bafeacf0f7b58a9c7492533
oleno <pirit.lena@gmail.com>
2017-02-28 15:11:07 +0200

    Recipe 10. Using Image Buttons

--- R10_Using_Image_Buttons/app/src/main/java/com/prt/r10_using_image_buttons/MainActivity.java
+++ R10_Using_Image_Buttons/app/src/main/java/com/prt/r10_using_image_buttons/MainActivity.java
@@ -2,6 +2,7 @@ package com.prt.r10_using_image_buttons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // adding text to Button 6 programmatically
+        Button bttn = (Button) findViewById(R.id.imageTextButton6);
+        bttn.setText("Android");
+        bttn.setCompoundDrawablesWithIntrinsicBounds(
+                0,                        // left
+                R.drawable.ic_launcher,   // top
+                0,                        // right
+                0);                       // bottom
     }
 }
