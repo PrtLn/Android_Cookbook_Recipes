c59a84b34cce38e0fcff6a4d58f6874848a165a3
oleno <pirit.lena@gmail.com>
2017-02-28 11:06:06 +0200

    Recipe 08. Making An Application Callable By Others

---
+++ R08_Making_App_Callable_By_Others/app/src/main/java/com/prt/r08_making_app_callable_by_others/MainActivity.java
@@ -0,0 +1,13 @@
+package com.prt.r08_making_app_callable_by_others;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
