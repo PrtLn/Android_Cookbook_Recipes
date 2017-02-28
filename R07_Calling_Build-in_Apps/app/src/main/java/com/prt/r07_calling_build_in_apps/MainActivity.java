836ed44cfb35755e32de1dbb2d8e0f9b7cb7c740
oleno <pirit.lena@gmail.com>
2017-02-28 08:24:47 +0200

    Recipe 07. Calling Build-in Apps (initial)

---
+++ R07_Calling_Build-in_Apps/app/src/main/java/com/prt/r07_calling_build_in_apps/MainActivity.java
@@ -0,0 +1,13 @@
+package com.prt.r07_calling_build_in_apps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
