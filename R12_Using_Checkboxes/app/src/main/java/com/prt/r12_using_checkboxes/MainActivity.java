c2833eab196ea8464b51030080c9961fb32b15df
oleno <pirit.lena@gmail.com>
2017-03-01 09:26:58 +0200

    Recipe 12. Using Checkboxes

---
+++ R12_Using_Checkboxes/app/src/main/java/com/prt/r12_using_checkboxes/MainActivity.java
@@ -0,0 +1,13 @@
+package com.prt.r12_using_checkboxes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
