f85d9634a8ac1f44f9e1307f10fd04b3b011530c
oleno <pirit.lena@gmail.com>
2017-03-01 09:42:31 +0200

    Recipe 12. Using CheckBoxes

--- R12_Using_Checkboxes/app/src/main/java/com/prt/r12_using_checkboxes/MainActivity.java
+++ R12_Using_Checkboxes/app/src/main/java/com/prt/r12_using_checkboxes/MainActivity.java
@@ -2,6 +2,9 @@ package com.prt.r12_using_checkboxes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // handled the CheckBox
+        CheckBox checkBox = (CheckBox) findViewById(R.id.checkAutosave);
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (((CheckBox)view).isChecked()) {
+                    Toast.makeText(getBaseContext(),
+                            "CheckBox is checked!",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getBaseContext(),
+                            "CheckBox is unchecked!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
