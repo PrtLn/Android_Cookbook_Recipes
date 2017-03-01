50f0e4dab3c237c1867bf5e695aa35e2428b8565
oleno <pirit.lena@gmail.com>
2017-03-01 09:23:26 +0200

    Recipe 11. Using Radio Buttons

--- R11_Using_Radio_Buttons/app/src/main/java/com/prt/r11_using_radio_buttons/MainActivity.java
+++ R11_Using_Radio_Buttons/app/src/main/java/com/prt/r11_using_radio_buttons/MainActivity.java
@@ -2,6 +2,9 @@ package com.prt.r11_using_radio_buttons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // handled the RadioButton
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.rgbGp1);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton rb1 = (RadioButton) findViewById(R.id.rgb1);
+                if (rb1.isChecked()) {
+                    Toast.makeText(getBaseContext(),
+                            "Option 1 checked!",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getBaseContext(),
+                            "Option 2 checked!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
