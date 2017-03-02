f9a3b18df2485b0d509edb9f1999099b323e7b65
oleno <pirit.lena@gmail.com>
2017-03-02 08:22:46 +0200

    Recipe 15. Displaying Web Pages (initial)

---
+++ R15_Displaying_Web_Pages/app/src/main/java/com/prt/r15_displaying_web_pages/MainActivity.java
@@ -0,0 +1,13 @@
+package com.prt.r15_displaying_web_pages;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
