d45c7b4a712a715361d0a1d5c3b37ea328f4333a
oleno <pirit.lena@gmail.com>
2017-03-02 08:55:09 +0200

    Recipe 15. Displaying Web Pages (an image loading from web)

--- R15_Displaying_Web_Pages/app/src/main/java/com/prt/r15_displaying_web_pages/MainActivity.java
+++ R15_Displaying_Web_Pages/app/src/main/java/com/prt/r15_displaying_web_pages/MainActivity.java
@@ -2,6 +2,10 @@ package com.prt.r15_displaying_web_pages;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.WebView01);
+        webView.setWebViewClient(new Callback());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setBuiltInZoomControls(true);
+
+        webView.loadUrl("http://cgimg.s3.amazonaws.com/cg/g62/54062/54062_1315855037_orig.jpg");
+    }
+
+    private class Callback extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return (false);
+        }
     }
 }
