cc4742c2c405a2d0f81470eeef24a16d4f3448ad
oleno <pirit.lena@gmail.com>
2017-03-02 09:45:29 +0200

    Recipe 16. Using The TimePicker

--- R16_Using_The_TimePicker/app/src/main/java/com/prt/r16_using_the_timepicker/MainActivity.java
+++ R16_Using_The_TimePicker/app/src/main/java/com/prt/r16_using_the_timepicker/MainActivity.java
@@ -1,13 +1,42 @@
 package com.prt.r16_using_the_timepicker;
 
+import android.icu.text.DecimalFormat;
+import android.icu.text.NumberFormat;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        timePicker.setIs24HourView(true);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void btnClick(View view) {
+        NumberFormat formatter = new DecimalFormat("00");
+        Toast.makeText(getBaseContext(),
+                "Time selected:" +
+        timePicker.getHour() +
+        ":" + formatter.format(timePicker.getMinute()),
+                Toast.LENGTH_SHORT).show();
+
+        /*
+        if (Build.VERSION.SDK_INT >= 23 )
+            myTimePickerView.getHour();
+        else
+            myTimePickerView.getCurrentHour();
+        */
     }
 }
