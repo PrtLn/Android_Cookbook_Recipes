4b044d537e36dfbe9e1654067f44168030290011
oleno <pirit.lena@gmail.com>
2017-03-02 10:29:01 +0200

    Recipe 17. Using The DatePicker

--- R17_Using_The_DatePicker/app/src/main/java/com/prt/r17_using_the_datepicker/MainActivity.java
+++ R17_Using_The_DatePicker/app/src/main/java/com/prt/r17_using_the_datepicker/MainActivity.java
@@ -2,12 +2,27 @@ package com.prt.r17_using_the_datepicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
+    }
+
+    public void btnClick(View view) {
+        Toast.makeText(getBaseContext(),
+                "Date selected: " + (datePicker.getMonth() + 1) +
+                                "/" + datePicker.getDayOfMonth() +
+                                "/" + datePicker.getYear(),
+                Toast.LENGTH_SHORT).show();
     }
 }
