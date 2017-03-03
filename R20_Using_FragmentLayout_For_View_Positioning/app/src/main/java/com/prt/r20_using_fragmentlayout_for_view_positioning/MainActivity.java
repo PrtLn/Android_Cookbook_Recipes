dd9bcd6bacf9bc0765aa7437eb2e1afd1da2d952
oleno <pirit.lena@gmail.com>
2017-03-03 10:23:25 +0200

    Recipe 20. Using FragmentLayout for View Positioning

--- R20_Using_FragmentLayout_For_View_Positioning/app/src/main/java/com/prt/r20_using_fragmentlayout_for_view_positioning/MainActivity.java
+++ R20_Using_FragmentLayout_For_View_Positioning/app/src/main/java/com/prt/r20_using_fragmentlayout_for_view_positioning/MainActivity.java
@@ -2,12 +2,37 @@ package com.prt.r20_using_fragmentlayout_for_view_positioning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int count = 1;
+    FrameLayout frame;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frame = (FrameLayout) findViewById(R.id.framelayout);
+    }
+
+    public void btnClick(View view) {
+        // hide the current one
+        imageView = (ImageView) frame.findViewWithTag(String.valueOf(count));
+        imageView.setVisibility(View.INVISIBLE);
+
+        // go to the next image
+        count++;
+        if (count > 3) {
+            count = 1;
+        }
+
+        // show the next image
+        imageView = (ImageView) frame.findViewWithTag(String.valueOf(count));
+        imageView.setVisibility(View.VISIBLE);
     }
 }
