cd91d0e5b6756d0140b401bf68e1a1a7b7543519
oleno <pirit.lena@gmail.com>
2017-03-04 08:30:14 +0200

    Recipe 23. Displaying Context And Options Menus

--- R23_Displaying_Context_And_Options_Menus/app/src/main/java/com/prt/r23_displaying_context_and_options_menus/MainActivity.java
+++ R23_Displaying_Context_And_Options_Menus/app/src/main/java/com/prt/r23_displaying_context_and_options_menus/MainActivity.java
@@ -2,6 +2,12 @@ package com.prt.r23_displaying_context_and_options_menus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button1);
+        btn.setOnCreateContextMenuListener(this);
+    }
+
+    // Options Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    public void onGroupItemClick(MenuItem item) {
+        if (item.isChecked()) {
+            item.setChecked(false);
+        } else {
+            item.setChecked(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    // Context Menu
+    @Override
+    public void onCreateContextMenu(ContextMenu menu,
+                                    View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_LONG).show();
+        return true;
     }
 }
