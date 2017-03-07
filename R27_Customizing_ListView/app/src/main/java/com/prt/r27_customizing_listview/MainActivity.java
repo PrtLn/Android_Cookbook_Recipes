a15e2fe272bf2d88f3e28f9d31270ce8cf58d14a
oleno <pirit.lena@gmail.com>
2017-03-07 15:57:48 +0200

    Recipe 27. Customizing the ListView

--- R27_Customizing_ListView/app/src/main/java/com/prt/r27_customizing_listview/MainActivity.java
+++ R27_Customizing_ListView/app/src/main/java/com/prt/r27_customizing_listview/MainActivity.java
@@ -1,13 +1,51 @@
 package com.prt.r27_customizing_listview;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
+
+    String[] cities = {
+            "New York",
+            "London",
+            "Toronto",
+            "Paris",
+            "Stockholm",
+            "San Francisco",
+            "Singapore",
+            "Hong Kong",
+            "Chicago",
+            "Tokyo"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = getListView();
+//        listView.setChoiceMode(ListView.CHOICE_MODE_NONE);
+//        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+        
+        // determines the item when printing by keyboard
+        listView.setTextFilterEnabled(true);
+
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_checked, cities));
+    }
+
+    public void onListItemClick(ListView parent, View view,
+                                int position, long id) {
+        CheckedTextView item = (CheckedTextView) view;
+        Toast.makeText(this, cities[position] + " checked : " +
+                        !item.isChecked(), Toast.LENGTH_SHORT).show();
     }
 }
