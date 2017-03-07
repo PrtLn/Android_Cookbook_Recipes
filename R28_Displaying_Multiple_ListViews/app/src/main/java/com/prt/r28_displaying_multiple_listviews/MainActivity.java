ed770bbe0ca5b15bc1b621120ff8b6b9833a6ab4
oleno <pirit.lena@gmail.com>
2017-03-07 17:08:48 +0200

    Recipe 28. Displaying Multiple ListViews

--- R28_Displaying_Multiple_ListViews/app/src/main/java/com/prt/r28_displaying_multiple_listviews/MainActivity.java
+++ R28_Displaying_Multiple_ListViews/app/src/main/java/com/prt/r28_displaying_multiple_listviews/MainActivity.java
@@ -2,12 +2,60 @@ package com.prt.r28_displaying_multiple_listviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView list1, list2;
+    String[] cities = {
+            "New York",
+            "London",
+            "Toronto",
+            "Paris",
+            "Stockholm",
+            "San Francisco",
+            "Singapore",
+            "Hong Kong",
+            "Chicago",
+            "Tokyo"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, cities);
+
+//        // List View
+        list1 = (ListView) findViewById(R.id.ListView1);
+        list1.setAdapter(adapter);
+        list1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                int index = arg2;
+                Toast.makeText(getBaseContext(),
+                        "You have selected item : " + cities[index],
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // List View
+        list2 = (ListView) findViewById(R.id.ListView2);
+        list2.setAdapter(adapter);
+        list2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                int index = arg2;
+                Toast.makeText(getBaseContext(),
+                        "You have selected item : " + cities[index],
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
