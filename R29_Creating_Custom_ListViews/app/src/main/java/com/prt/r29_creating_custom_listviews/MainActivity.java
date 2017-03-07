d0248bc1bfb63f86e65b6f59c9db194979bad9e1
oleno <pirit.lena@gmail.com>
2017-03-07 17:52:46 +0200

    Recipe 29. Creating Custom ListViews

--- R29_Creating_Custom_ListViews/app/src/main/java/com/prt/r29_creating_custom_listviews/MainActivity.java
+++ R29_Creating_Custom_ListViews/app/src/main/java/com/prt/r29_creating_custom_listviews/MainActivity.java
@@ -1,13 +1,33 @@
 package com.prt.r29_creating_custom_listviews;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
+
+    String[] cities = {
+            "New York",
+            "London",
+            "Toronto",
+            "Paris",
+            "Stockholm",
+            "San Francisco",
+            "Singapore",
+            "Hong Kong",
+            "Chicago",
+            "Tokyo"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setListAdapter(new ArrayAdapter<String>(
+                this,
+                R.layout.leftrowlayout,
+                R.id.txtCitiesName,
+                cities));
     }
 }
