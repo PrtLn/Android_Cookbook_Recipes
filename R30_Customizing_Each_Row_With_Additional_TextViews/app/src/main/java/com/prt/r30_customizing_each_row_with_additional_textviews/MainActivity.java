aade61bc19c3a5aee40876156b0b5520fb619879
oleno <pirit.lena@gmail.com>
2017-03-08 13:24:20 +0200

    Recipe 30. Further Customizing Each Row With Additional TextViews (initial)

---
+++ R30_Customizing_Each_Row_With_Additional_TextViews/app/src/main/java/com/prt/r30_customizing_each_row_with_additional_textviews/MainActivity.java
@@ -0,0 +1,13 @@
+package com.prt.r30_customizing_each_row_with_additional_textviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
