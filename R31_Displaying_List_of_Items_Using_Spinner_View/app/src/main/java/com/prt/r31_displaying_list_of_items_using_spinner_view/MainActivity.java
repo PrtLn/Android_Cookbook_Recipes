266c3a7e452829bf4fc61418ae36db0e4c4db1ae
oleno <pirit.lena@gmail.com>
2017-03-27 14:21:06 +0300

    Recipe 31. Displaying a List of Items Using Spinner View

--- R31_Displaying_List_of_Items_Using_Spinner_View/app/src/main/java/com/prt/r31_displaying_list_of_items_using_spinner_view/MainActivity.java
+++ R31_Displaying_List_of_Items_Using_Spinner_View/app/src/main/java/com/prt/r31_displaying_list_of_items_using_spinner_view/MainActivity.java
@@ -2,12 +2,50 @@ package com.prt.r31_displaying_list_of_items_using_spinner_view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner s1;
+    String[] cities = {
+            "New York",
+            "London",
+            "Toronto",
+            "Paris",
+            "Stockholm",
+            "San Francisco",
+            "Singapore",
+            "Hong Kong",
+            "Chicago",
+            "Tokyo"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Spinner View
+        s1 = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, cities);
+        s1.setAdapter(adapter);
+
+        s1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                int index = s1.getSelectedItemPosition();
+                Toast.makeText(getBaseContext(),
+                        "You have selected item : " + cities[index],
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
     }
 }
