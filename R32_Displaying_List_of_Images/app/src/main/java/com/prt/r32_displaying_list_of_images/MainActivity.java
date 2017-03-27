b73495fa6770894628a1fbc4bf32ec16405ff529
oleno <pirit.lena@gmail.com>
2017-03-27 15:52:43 +0300

    Recipe 32. Displaying a List of Images

--- R32_Displaying_List_of_Images/app/src/main/java/com/prt/r32_displaying_list_of_images/MainActivity.java
+++ R32_Displaying_List_of_Images/app/src/main/java/com/prt/r32_displaying_list_of_images/MainActivity.java
@@ -1,13 +1,96 @@
 package com.prt.r32_displaying_list_of_images;
 
+import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+@SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity {
+
+    // the images to display
+    Integer[] imageIDs = {
+            R.drawable.pic1,
+            R.drawable.pic2,
+            R.drawable.pic3,
+            R.drawable.pic4,
+            R.drawable.pic5,
+            R.drawable.pic6,
+            R.drawable.pic7,
+            R.drawable.pic8
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Gallery view is deprecated in Android 4.1 and above (using @SuppressWarnings annotation)
+        Gallery gallery = (Gallery) findViewById(R.id.gallery1);
+        gallery.setAdapter(new ImageAdapter(this));
+        gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(),
+                        "picture " + (position + 1) + " selected",
+                        Toast.LENGTH_SHORT).show();
+
+                // display the images selected
+                ImageView imageView = (ImageView) findViewById(R.id.image1);
+                imageView.setImageResource(imageIDs[position]);
+            }
+        });
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+
+        private Context context;
+        private int itemBackground;
+
+        public ImageAdapter(Context c) {
+
+            context = c;
+
+            //sets a gray background; wraps around the images
+            TypedArray a = obtainStyledAttributes(R.styleable.MyGallery);
+            itemBackground = a.getResourceId(R.styleable.MyGallery_android_galleryItemBackground, 0);
+            a.recycle();
+        }
+
+        // return the number of images
+        @Override
+        public int getCount() {
+            return imageIDs.length;
+        }
+
+        // return the ID of an item
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        // return the ID of an item
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        // return an ImageView view
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = new ImageView(context);
+            imageView.setImageResource(imageIDs[position]);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            imageView.setLayoutParams(new Gallery.LayoutParams(250, 220));
+            imageView.setBackgroundResource(itemBackground);
+            return imageView;
+        }
     }
 }
